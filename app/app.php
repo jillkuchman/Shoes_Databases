2d8080711acb5edcc21b8f27b83f9cb764ed6a36
Jill Kuchman <jillkuchman@gmail.com>
2015-03-27 11:28:08 -0700

    Add get, post routes for /brands, /brands/{id}

--- app/app.php
+++ app/app.php
@@ -22,33 +22,60 @@
     });
 
 
-    //STORES pages, list of stores with links to each store, can edit each store from individual page
+    //STORES pages
+    //STORES page, GET: list of stores with links to each store, form to add a new Store
     $app->get("/stores", function() use($app) {
-        return $app['twig']->render('stores.html.twig', array('stores' = Store::getAll()));
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
     });
 
+    //STORES page, POST: adds a new store to list of stores
     $app->post("/stores", function() use($app) {
         $new_store = new Store($_POST['name']);
         $new_store->save();
-        return $app['twig']->render('stores.html.twig', array('stores' = Store::getAll()));
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
     });
 
-    //Individual Store pages
+    //INDIVIDUAL STORE PAGES
     //Get: loads store page with list of brands the store carries and the option to add a brand from the list of brands
     $app->get("/stores/{id}", function($id) use($app) {
         $current_store = Store::find($id);
         return $app['twig']->render('store.html.twig', array('store' => $current_store, 'brands' => $current_store->getBrands(), 'all_brands' => Brand::getAll()));
     });
 
-    //Post: adds the brand that was selected in the Get page to the current store's list of brands, submitted from a form with the name "brand_id"
+    //Post: adds the brand that was selected in the Get route to the current store's list of brands, submitted from a form with the name "brand_id"
     $app->post("/stores/{id}", function($id) use($app) {
         $current_store = Store::find($id);
-        $new_brand = Brand::find($_POST['brand_id'])
+        $new_brand = Brand::find($_POST['brand_id']);
         $current_store->addBrand($new_brand);
         return $app['twig']->render('store.html.twig', array('store' => $current_store, 'brands' => $current_store->getBrands(), 'all_brands' => Brand::getAll()));
     });
 
+    //BRANDS pages
 
+    //BRANDS page, GET: list of brands with links to each brand, form to add new Brand
+    $app->get("/brands", function() use($app) {
+        return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
+    });
+
+    //BRANDS page, POST: adds a new brand to list of brands
+    $app->post("/brands", function() use($app) {
+        return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
+    });
+
+    //INDIVIDUAL BRAND PAGES
+    //Get: loads brand page with list of stores that carry the brand, and the option to add a store to the list of stores
+    $app->get("/brands/{id}", function($id) use($app) {
+        $current_brand = Brand::find($id);
+        return $app['twig']->render('brand.html.twig', array('brand' => $current_brand, 'stores' => $current_brand->getStores(), 'all_stores' => Store::getAll()));
+    });
+
+    //Post: adds the store that was selected in the Get route to the current brand's list of stores, submitted from a form with the name "store_id"
+    $app->post("/brands/{id}", function($id) use($app) {
+        $current_brand = Brand::find($id);
+        $new_store = Store::find($_POST['store_id']);
+        $current_brand->addStore($new_store);
+        return $app['twig']->render('brand.html.twig', array('brand' => $current_brand, 'stores' => $current_brand->getStores(), 'all_stores' => Store::getAll()));
+    });
 
     return $app;
 ?>
